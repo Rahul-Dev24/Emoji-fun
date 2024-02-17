93bb5cf68f3f1e9f57ce46bcaea016d02ff46307
Rahul-Dev24 <rahulsingh908714@gmail.com>
2024-02-17 23:51:10 +0530

    Emoji-complited

---
+++ script.js
@@ -0,0 +1,44 @@
+// emoji switching
+
+(() => {
+  setTimeout(() => {
+    document.querySelector(".container").style.display = "none";
+    document.querySelector(".container2").style.display = "flex";
+  }, 13000);
+  setTimeout(() => {
+    document.querySelector(".container").style.display = "none";
+    document.querySelector(".container2").style.display = "none";
+    document.querySelector(".container3").style.display = "flex";
+  }, 25000);
+
+  setTimeout(() => {
+    document.querySelector(".container").style.display = "none";
+    document.querySelector(".container2").style.display = "none";
+    document.querySelector(".container3").style.display = "none";
+    document.querySelector(".container4").style.display = "flex";
+  }, 35000);
+  setTimeout(() => {
+    document.querySelector(".container").style.display = "none";
+    document.querySelector(".container2").style.display = "none";
+    document.querySelector(".container3").style.display = "none";
+    document.querySelector(".container4").style.display = "none";
+    document.querySelector(".container6").style.display = "flex";
+  }, 40000);
+  setTimeout(() => {
+    document.querySelector(".container").style.display = "none";
+    document.querySelector(".container2").style.display = "none";
+    document.querySelector(".container3").style.display = "none";
+    document.querySelector(".container4").style.display = "none";
+    document.querySelector(".container6").style.display = "none";
+    document.querySelector(".container5").style.display = "flex";
+  }, 50000);
+  setTimeout(() => {
+    document.querySelector(".container").style.display = "none";
+    document.querySelector(".container2").style.display = "none";
+    document.querySelector(".container3").style.display = "none";
+    document.querySelector(".container4").style.display = "none";
+    document.querySelector(".container6").style.display = "none";
+    document.querySelector(".container5").style.display = "none";
+    document.querySelector(".container9").style.display = "flex";
+  }, 55000);
+})();
